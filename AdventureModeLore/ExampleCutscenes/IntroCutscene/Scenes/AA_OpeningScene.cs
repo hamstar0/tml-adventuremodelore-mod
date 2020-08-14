3296ccf53e5b8da5c901b485a9689eb6e8ab0994
hamstar0 <webutek@gmail.com>
2020-08-14 16:54:52 -0700

    v0.4.1 - Begun adding camera sequence

--- AdventureModeLore/ExampleCutscenes/IntroCutscene/Scenes/AA_OpeningScene.cs
+++ AdventureModeLore/ExampleCutscenes/IntroCutscene/Scenes/AA_OpeningScene.cs
@@ -1,6 +1,7 @@
 ﻿using System;
+using Microsoft.Xna.Framework;
 using Terraria;
-using HamstarHelpers.Services.Camera;
+using HamstarHelpers.Classes.CameraAnimation;
 using HamstarHelpers.Helpers.Debug;
 using AdventureModeLore.Definitions;
 
@@ -13,16 +14,40 @@ namespace AdventureModeLore.ExampleCutscenes.Intro.Scenes {
 		////////////////
 
 		protected override void OnBeginOnPlayer( IntroCutscene parent, Player player ) {
-			CameraMover.Current = new CameraMover(
+			CameraMover cam1 = null, cam2 = null, cam3 = null;
+
+			Vector2 exteriorShipStartPos = parent.CurrentPosition;
+
+			bool isShipOnLeft = (int)exteriorShipStartPos.X < ((16 * Main.maxTilesX) / 2);
+
+			Vector2 dungeonViewPos = new Vector2( Main.dungeonX * 16, Main.dungeonY * 16 );
+			dungeonViewPos.X += isShipOnLeft ? (-32 * 16) : (32 * 16);
+			dungeonViewPos.Y += -40 * 16;
+
+			cam1 = new CameraMover(
 				name: "AdventureModeIntro",
-				moveXFrom: (int)parent.CurrentPosition.X,
-				moveYFrom: (int)parent.CurrentPosition.Y,
-				moveXTo: (int)parent.CurrentPosition.X,
-				moveYTo: (int)parent.CurrentPosition.Y - (12 * 16),
+				moveXFrom: (int)exteriorShipStartPos.X,
+				moveYFrom: (int)exteriorShipStartPos.Y,
+				moveXTo: (int)exteriorShipStartPos.X,
+				moveYTo: (int)exteriorShipStartPos.Y - (12 * 16),
 				toDuration: 60 * 3,
 				lingerDuration: 60 * 3,
-				froDuration: 0
+				froDuration: 0,
+				onStop: () => CameraMover.Current = cam2
 			);
+			cam2 = new CameraMover(
+				name: "AdventureModeIntro",
+				moveXFrom: (int)dungeonViewPos.X,
+				moveYFrom: (int)dungeonViewPos.Y,
+				moveXTo: (int)dungeonViewPos.X,
+				moveYTo: (int)dungeonViewPos.Y - (4 * 16),
+				toDuration: 60 * 5,
+				lingerDuration: 0,
+				froDuration: 0
+				//onStop: () => CameraMover.Current = cam3
+			);
+
+			CameraMover.Current = cam1;
 		}
 
 		////////////////
