9c3dfa822486f85a44e45b8d6024e64769d8b3c8
hamstar0 <webutek@gmail.com>
2020-08-17 02:38:08 -0700

    v0.4.3 - Relocated ActiveCutscene to its own non-nested class

---
+++ AdventureModeLore/Definitions/ActiveCutscene.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Terraria;
+using HamstarHelpers.Helpers.Debug;
+
+
+namespace AdventureModeLore.Definitions {
+	public abstract partial class ActiveCutscene {
+		public Cutscene Parent { get; }
+
+		public int PlaysForWhom { get; }
+
+		////
+
+		public int CurrentSceneIdx { get; internal set; } = 0;
+
+
+
+		////////////////
+
+		public ActiveCutscene( Cutscene parent, Player playsFor, int sceneIdx ) {
+			this.Parent = parent;
+			this.PlaysForWhom = playsFor.whoAmI;
+			this.CurrentSceneIdx = sceneIdx;
+		}
+
+		public abstract ActiveCutscene Clone();
+
+
+		////////////////
+
+		public virtual void OnEnd_World() { }
+
+		public virtual void OnEnd_Player( Player player ) { }
+
+
+		////////////////
+
+		public virtual bool Update_World() {
+			return false;
+		}
+
+		public virtual bool Update_Player( Player player ) {
+			return false;
+		}
+	}
+}
---
+++ AdventureModeLore/Definitions/ActiveCutscene_Scene.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Terraria;
+using Terraria.ID;
+using HamstarHelpers.Helpers.Debug;
+using AdventureModeLore.Logic;
+using AdventureModeLore.Net;
+
+
+namespace AdventureModeLore.Definitions {
+	public abstract partial class ActiveCutscene {
+		internal void SetCurrentScene_Player( Player player, int sceneIdx ) {
+			if( this.CurrentSceneIdx == sceneIdx ) {
+				return;
+			}
+
+			Scene prevScene = this.Parent.Scenes[this.CurrentSceneIdx];
+			prevScene.End_Player_Internal( this.Parent, player );
+
+			this.CurrentSceneIdx = sceneIdx;
+
+			Scene currScene = this.Parent.Scenes[this.CurrentSceneIdx];
+			currScene.Begin_Player_Internal( this.Parent, player );
+		}
+
+		internal void SetCurrentScene_World( int sceneIdx ) {
+			if( this.CurrentSceneIdx == sceneIdx ) {
+				return;
+			}
+
+			Scene prevScene = this.Parent.Scenes[this.CurrentSceneIdx];
+			prevScene.End_World_Internal( this.Parent );
+
+			this.CurrentSceneIdx = sceneIdx;
+
+			Scene currScene = this.Parent.Scenes[this.CurrentSceneIdx];
+			currScene.Begin_World_Internal( this.Parent );
+		}
+
+
+		////////////////
+
+		public bool CanAdvanceCurrentScene_Any() {
+			Scene scene = this.Parent.Scenes[this.CurrentSceneIdx];
+
+			return ( scene.DefersToHostForSync && Main.netMode == NetmodeID.Server )
+				|| ( !scene.DefersToHostForSync && Main.netMode == NetmodeID.MultiplayerClient );
+		}
+
+		public bool AdvanceScene_Any( bool sync ) {
+			Player playingFor = Main.player[this.PlayingForWho];
+			if( playingFor == null ) {
+				return false;
+			}
+
+			this.CurrentSceneIdx++;
+
+			if( this.CurrentSceneIdx < this.Parent.Scenes.Length ) {
+				if( sync ) {
+					if( Main.netMode == NetmodeID.MultiplayerClient ) {
+						AMLCutsceneNetData.Broadcast( playingFor, this.Parent, this.CurrentSceneIdx );
+					} else if( Main.netMode == NetmodeID.Server ) {
+						AMLCutsceneNetData.SendToClients( playingFor, - 1, this.Parent, this.CurrentSceneIdx );
+					}
+				}
+			} else {
+				CutsceneManager.Instance.EndCutscene_Any( this.Parent.UniqueId, sync );
+			}
+
+			return true;
+		}
+	}
+}
