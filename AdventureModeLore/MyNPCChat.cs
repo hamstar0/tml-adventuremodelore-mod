62b9d17502a4c7ca0c3d2f2465920e83a7b5cf31
hamstar0 <webutek@gmail.com>
2020-12-13 11:07:54 -0800

    v0.5.1 - Added dialogue for nurse and arms dealer

--- AdventureModeLore/MyNPCChat.cs
+++ AdventureModeLore/MyNPCChat.cs
@@ -1,85 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
 using Terraria;
 using Terraria.ID;
 using HamstarHelpers.Classes.Loadable;
+using HamstarHelpers.Helpers.Debug;
 using HamstarHelpers.Helpers.DotNET.Extensions;
 using HamstarHelpers.Helpers.TModLoader;
-using HamstarHelpers.Helpers.Debug;
 using HamstarHelpers.Services.Dialogue;
 
 
 namespace AdventureModeLore {
-	public class NPCDialogueDefinitions {
-		public string[] Added { get; }
-		public string[] Blocked { get; }
-
-
-
-		public NPCDialogueDefinitions( string[] added, string[] blocked ) {
-			this.Added = added;
-			this.Blocked = blocked;
-		}
-	}
-
-
-
-
 	partial class AdventureModeNpcChat : ILoadable {
-		public readonly IDictionary<int, NPCDialogueDefinitions> NPCDialogues = new Dictionary<int, NPCDialogueDefinitions> {
-			{
-				NPCID.Guide, new NPCDialogueDefinitions(
-					added: new string[] {
-						"Good thing the dungeon is sealed. I hear it's blighted with an undeath curse and filled with deadly fumes!",
-						//"The people who lived here once discovered ways to wield artifacts of power, and hid their secrets around this land.",
-						"Rare magic crystals can be found hidden underground. Use your binoculars to pick up their trail. I also hear they resonate with nearby magical spell casting.",
-						"You may find digging to be rather difficult. If you find yourself needing to squeeze into tight areas, a simple hammer of all things might be your best tool. Odd, huh?",
-						"Not everything can be crafted. You'll have to learn to make do with what you can find or buy. Talk to me for more information, if in doubt.",
-						"Need to add a few small patches or solid additions to a given area? You'll want to get your hands on some framing planks.",
-						"Be sure to keep your eyes peeled for livable spaces to furnish. This is where our House Furnishing Kits will come in handy. They'll even provide you with new storage space and mirrors for fast travel!",
-						"Wood is about the only non-ore material you can break freely. You'll need special framing planks to do any building, but they're use is limited. Best use House Framing Kits whenever possible.",
-						"Need money? Sell your loot. Be sure to have me check your loot for available crafting recipes, first. Not everything gets used the way you might assume.",
-						//
-						"You can use your axe to chop down trees. Just place your cursor over the tile and click!",
-						"We'll need to create settlements to progress our journey. Use House Furnishing Kits to convert closed areas into livable spaces.",
-					},
-					blocked: new string[] {
-						"You can use your pickaxe to dig through dirt",
-						"If you want to survive",
-						"When you have enough wood, create a workbench",
-						"You can build a shelter by placing wood or other blocks in the world",
-						"Once you have a wooden sword,",
-						"To interact with backgrounds, use a hammer",
-						"You can create a furnace out of torches, wood, and stone",
-						"Anvils can be crafted out of iron",
-						"they can be combined to create an item that will increase your magic capacity",
-						"The ebonstone in the corruption can be purified",
-						"You should make an attempt to max out your available life",
-						"You can make a grappling hook from a hook",
-					}
-				)
-			},
-			{
-				NPCID.Dryad, new NPCDialogueDefinitions(
-					added: new string[] {
-						"Ever encounter that annoying Trickster? I hear it likes to reward those who think they can outwit it with quick thinking. I think it's up to something...",
-					},
-					blocked: new string[] {
-					}
-				)
-			}
-		};
-
-
-
-		////////////////
-
 		public void OnModsLoad() { }
 
 		public void OnPostModsLoad() {
 			foreach( (int npcType, NPCDialogueDefinitions chats) in this.NPCDialogues ) {
 				foreach( string addedChat in chats.Added ) {
-					DialogueEditor.AddChatForNPC( npcType, addedChat, 0.1f );
+					if( chats.IsAvailable?.Invoke(addedChat) ?? true ) {
+						DialogueEditor.AddChatForNPC( npcType, addedChat, 0.1f );
+					}
 				}
 				foreach( string blockedChat in chats.Blocked ) {
 					DialogueEditor.AddChatRemoveFlatPatternForNPC( npcType, blockedChat );
---
+++ AdventureModeLore/MyNPCChat_Defs.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using HamstarHelpers.Classes.Loadable;
+using HamstarHelpers.Helpers.Debug;
+
+
+namespace AdventureModeLore {
+	public class NPCDialogueDefinitions {
+		public string[] Added { get; }
+		public string[] Blocked { get; }
+
+		public Func<string, bool> IsAvailable { get; }
+
+
+
+		public NPCDialogueDefinitions( string[] added, string[] blocked, Func<string, bool> isAvailable=null ) {
+			this.Added = added;
+			this.Blocked = blocked;
+			this.IsAvailable = isAvailable;
+		}
+	}
+
+
+
+
+	partial class AdventureModeNpcChat : ILoadable {
+		public readonly IDictionary<int, NPCDialogueDefinitions> NPCDialogues = new Dictionary<int, NPCDialogueDefinitions> {
+			{
+				NPCID.Guide, new NPCDialogueDefinitions(
+					added: new string[] {
+						"Good thing the dungeon is sealed. I hear it's blighted with an undeath curse and filled with deadly fumes!",
+						//"The people who lived here once discovered ways to wield artifacts of power, and hid their secrets around this land.",
+						"Rare magic crystals can be found hidden underground. Use your binoculars to pick up their trail. I also hear they resonate with nearby magical spell casting.",
+						"You may find digging to be rather difficult. If you find yourself needing to squeeze into tight areas, a simple hammer of all things might be your best tool. Odd, huh?",
+						"Not everything can be crafted. You'll have to learn to make do with what you can find or buy. Talk to me for more information, if in doubt.",
+						"Need to add a few small patches or solid additions to a given area? You'll want to get your hands on some framing planks.",
+						"Be sure to keep your eyes peeled for livable spaces to furnish. This is where our House Furnishing Kits will come in handy. They'll even provide you with new storage space and mirrors for fast travel!",
+						"Wood is about the only non-ore material you can break freely. You'll need special framing planks to do any building, but they're use is limited. Best use House Framing Kits whenever possible.",
+						"Need money? Sell your loot. Be sure to have me check your loot for available crafting recipes, first. Not everything gets used the way you might assume.",
+						//
+						"You can use your axe to chop down trees. Just place your cursor over the tile and click!",
+						"We'll need to create settlements to progress our journey. Use House Furnishing Kits to convert closed areas into livable spaces.",
+					},
+					blocked: new string[] {
+						"You can use your pickaxe to dig through dirt",
+						"If you want to survive",
+						"When you have enough wood, create a workbench",
+						"You can build a shelter by placing wood or other blocks in the world",
+						"Once you have a wooden sword,",
+						"To interact with backgrounds, use a hammer",
+						"You can create a furnace out of torches, wood, and stone",
+						"Anvils can be crafted out of iron",
+						"they can be combined to create an item that will increase your magic capacity",
+						"The ebonstone in the corruption can be purified",
+						"You should make an attempt to max out your available life",
+						"You can make a grappling hook from a hook",
+					}
+				)
+			},
+			{
+				NPCID.Nurse, new NPCDialogueDefinitions(
+					added: new string[] {
+						"I can heal bodily wounds, but this land does things to people that I cannot heal. Spiritual matters are "
+						+ "not my forté, though. You'll want a priest or witch doctor or something for that, I think. I'm not "
+						+ "being speaking figuratively. Do NOT let your spirits get down, or things here can harm you. If you "
+						+ "survive, I can help with that, though.",
+					},
+					blocked: new string[] {
+					},
+					isAvailable: ( msg ) => ModLoader.GetMod("Necrotis") != null
+				)
+			},
+			{
+				NPCID.Dryad, new NPCDialogueDefinitions(
+					added: new string[] {
+						"Ever encounter that annoying Trickster? I hear it likes to reward those who think they can outwit it with quick thinking. I think it's up to something...",
+					},
+					blocked: new string[] {
+					},
+					isAvailable: ( msg ) => ModLoader.GetMod("TheTrickster") != null
+				)
+			},
+			{
+				NPCID.ArmsDealer, new NPCDialogueDefinitions(
+					added: new string[] {
+						"That's a mighty fine hand cannon you brought with you. I've never seen one like it. Really though, "
+						+ "why not buy a real gun from me? Surely that old thing can't hit the broad side of a barn! Would "
+						+ "probably level one just fine, with whatever you added to the powder.",
+					},
+					blocked: new string[] {
+					},
+					isAvailable: ( msg ) => ModLoader.GetMod("TheMadRanger") != null
+				)
+			}
+		};
+	}
+}
