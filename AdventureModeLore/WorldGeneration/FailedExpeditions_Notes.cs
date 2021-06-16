02f807a1162dd82e3a523df934334f151a90e44b
hamstar0 <webutek@gmail.com>
2021-06-16 15:53:32 -0700

    v0.11.0 - Edited lore notes

--- AdventureModeLore/WorldGeneration/FailedExpeditions_Notes.cs
+++ AdventureModeLore/WorldGeneration/FailedExpeditions_Notes.cs
@@ -9,10 +9,11 @@ namespace AdventureModeLore.WorldGeneration {
 		public static readonly (string title, string[] pages)[] LoreNotes = {
 			( "The Plague", new string[] {
 				"Also known as 'The Curse', 'The Undeath Plague', and even ominously as 'Signs', this is"
-				+ "\na phenomena of recent years accounting for mysterious afflictions of madness of some,"
-				+ "\nwho also exhibit an unnatural resilience against dying.",
+				+ "\na phenomena of recent years to identify a mysterious afflictions of sickness and"
+				+ "\nmadness, but also an unnatural resilience against death.",
 				"At first attributed to an unknown virulent organism, we now know this to be something"
-				+ "\naltogether new to science and medicine, though apparently not to theology.",
+				+ "\naltogether new to science and medicine, though apparently not to history (and theoloy,"
+				+ "\napparently).",
 				"Ancient records of civilizations falling to mysterious 'feral behavior' maladies can be"
 				+ "\nfound, often coinciding with the equally-mysterious rise of new civilizations.",
 				"More pragmatically, the plague may be attributed simply to a viral source that yet"
@@ -22,116 +23,133 @@ namespace AdventureModeLore.WorldGeneration {
 			( "Ectoplasm", new string[] {
 				"The environmental effects of concentrations of \"negatively-charged spiritual residue\""
 				+ "\nare numerous. Commonly called Ectoplasm, this wispy substance can animate and alter"
-				+ "\nthings around it, often destructively.",
+				+ "\nthings around it, though often only destructively.",
 				"Large concentrations of naturally-occuring ectoplasm have been known to turn wildlife"
-				+ "\ninto vicious killers, re-animate corpses, and even give life to organic detritous or"
-				+ "\nother microbial matter (a phenomena often known as 'slimes').",
+				+ "\ninto rabid killing machines, re-animate corpses, and even give life to organic detritous"
+				+ "\nor microbial matter (a phenomena often known as 'slimes').",
 				"A large surge of this residue has recently been reported emanating from an island now"
 				+ "\nknown as Terraria. Its effects are being felt all around the world, and some believe"
-				+ "\nit to be the cause of the undeath plague. Modern science, however, has yet to"
-				+ "\ncorroborate on any of these assertions. Efforts to acquire, or even definitively"
-				+ "\nconfirm the presence of this substance, have so far proven unfruitful."
+				+ "\nit to be the cause of the plague. Modern science, however, has yet to corroborate on any"
+				+ "\nof these assertions. Efforts to acquire, or even definitively confirm the presence of"
+				+ "\nthis substance, have so far proven unfruitful."
 			} ),
 			( "The Island of Terraria", new string[] {
-				"The island of Terraria is a curious place. It is shrouded in much mystery, and although"
-				+ "\nindications of a long history are evident, few documented records exist to tell of it.",
-				"Upon arrival, one may note a curious conglomeration of varying extreme biomes, terrain,"
-				+ "\nand other elements. Their proximity and relative intensity of presence seem to altogether"
-				+ "\ndefy nature and plausibility.",
-				"Even more notable are the occurrence of elements that aren't even found in nature, and"
+				"The island of Terraria is a mysterious place. Although indications of a long history are"
+				+ "\nevident, few documented records exist to tell of it.",
+				"Upon arrival, one may note a curious conglomeration of varying extreme biomes, terrain"
+				+ "\nfeatures, and other elements. Their proximity and relative significance of presence seem"
+				+ "\nto defy nature and plausibility.",
+				"Even more notable are the occurrence of phenomena that aren't even found in nature, and"
 				+ "\nare yet to be fully understood by science.",
 				"Among these phenomena: A large biome of festering \"corruption\", giant trees towering"
 				+ "\nabove any of the island's native foliage, and even levitating pieces of terrain amidst"
-				+ "\nthe cloud.",
-				"Perhaps most curiously, there exists a single, colossal structure built of an material"
-				+ "\nunlike anything found anywhere in the world."
+				+ "\nthe clouds.",
+				"Perhaps most curiously, there exists a colossal, fortress-like structure reaching deep"
+				+ "\nunderground, and built of a material unlike anything found anywhere in the world."
+				+ "\nTeams of investigators have gone missing while attempting to explore it, and it's"
+				+ "\nmaterial composition seems nigh impervious to conventional explosives. It does not,"
+				+ "\nhowever, seem to be currently occupied, although reports exist of strange folk"
+				+ "\noccasionally seen near its entrance..."
 			} ),
 			( "The Lunatic Cult", new string[] {
 				"One of the few natives of the land of Terraria, this group acts as \"agents of ancient"
-				+ "\ncosmic powers seeking to create a new home for their elder god masters\", allegedly.",
-				"Witnesses say their gatherings are associated with all manner of sorcery, which they"
-				+ "\nclaim is fueled by a substance called \"ectoplasm\".",
-				"They claim to have build the large dungeon-like structure on the island, but they do"
-				+ "\nnot presently appear to reside there.",
-				"They also claim this structure is tied to the their sorcery, and by extension, their supply"
-				+ "\nof \"ectoplasm\".",
-				"They say this substance is produced from the spirits of the dead, which would strongly"
-				+ "\nsuggest this massive dungeon is rather a place of human sacrifice, rather than mere"
-				+ "\nincarceration.",
+				+ "\ncosmic powers seeking a new home for their elder god masters\", allegedly.",
+				"Witnesses say their gatherings are associated with all manner of sorcery and mysterious"
+				+ "\nhappenings, though rumors of human sacrifices so far appear to be unconfirmed.",
+				"They claim to have built the large \"dungeon\" structure on the island, but they do not"
+				+ "\npresently appear to reside there. They claim this structure is important to their"
+				+ "\nmission, and that it plays a role in their quest for \"ectoplasm\"; the apparent"
+				+ "\nfuel source for their sorcery.",
+				"They say this substance is produced from the spirits of the dead, which would suggest"
+				+ "\ntheir \"dungeon\" is not a place of mere incarceration...",
 				"Cultist gatherings and recruitments seem to be on the rise of late, as are ever more"
 				+ "\nexotic and disturbing tales of their acts of sorcery all across the island.",
-				"Could this have any bearing on the rise of the undeath plague in recent years?"
+				"Could this have any correlation with the rise of the undeath plague in recent years?"
 			} ),
 			( "The 'Lihzarhd' Tribe", new string[] {
-				"Another of the natives of the land, these are a hardy, humanoid tribe of survivors of"
-				+ "\nthe great war. They hid away as best they could to excape the Curse, but it still"
-				+ "\ntook its toll.",
-				"Rather than succumbing to madness (or worse) like most others, these people lost their"
-				+ "\nhuman forms, becoming lizard-like beasts of questionable-sapience.",
-				"They appear to reside deep within the island's jungle, hidden away in deep, uncharted"
-				+ "\nrefuges.",
-				"Rumor has it they seek some sort of revenge, but until proper contact can be made, this"
-				+ "\nclaim has yet to be verified.",
-				"Despite there being less sightings of them away from their jungle home lately, no"
-				+ "\nindication exists that their numbers have reduced.",
+				"Another tribe of natives of the land, these hardy humanoids appear to have survived in"
+				+ "\nhiding since the great war. Despite that, the Curse appears to have again taken its"
+				+ "\ntoll.",
+				"Rather than succumbing to illness or madness (or worse) like most others, these people"
+				+ "\nsimply lost their human forms, instead becoming lizard-like beasts.",
+				"They appear to reside deep within the island's jungle, hidden away in some uncharted"
+				+ "\nrefuge or other, somehow enduring against the plague even where its effects may be"
+				+ "\nfelt more than most.",
+				"Rumor has it they seek some sort of revenge, but as any proper contact has yet to be"
+				+ "\nmade with their tribe, this claim has yet to be verified.",
+				"They rarely leave their jungle home, and lately seem to be more active and reclusive"
+				+ "\nthan ever. Perhaps something connects this to the recent rise of cultist activities"
+				+ "\non the island?",
 				"Ignoring their appearance and behavior, they do seem to be quite intelligent, as they"
-				+ "\nare noted for creating elaborate traps all around the island, and even wielding"
-				+ "\nsurprisingly advanced technologies.",
-				"Rumors also exist that they may even be practicing sorcery in recent years, seemingly"
-				+ "\nobsessed with \"apocalyptic magics\" in particular."
+				+ "\nare noted for creating elaborate traps all around the island. Rumors also exist of"
+				+ "\ntheir use of advanced technology and magic, though they at least appear to prefer"
+				+ "\na more primitive lifestyle.",
+				"Reports also exist that they may even be practicing religion in recent years, with"
+				+ "\nemphasis on sun worship and strong allusions to a soon-coming apocalypse..."
 			} ),
 			( "The Goblin Army", new string[] {
 				"Yet more survivors of the Great War. They dwell in the shadows and far corners of the"
-				+ "\nisland. Also afflicted by the curse, similar to the \"lihzarhds\", they too have"
+				+ "\nisland. Also afflicted by the Curse, similar to the \"lihzarhds\", they too have"
 				+ "\nmutated from their original human forms.",
 				"Though they are always warring among themselves and their neighbors, it appears some"
 				+ "\nsemblance of society exists among them. They are known to be adept magicians, and"
-				+ "do not shy away from contact.",
-				"Direct communication, however, has proven unfruitful, as even their scouts frequently"
-				+ "\nbecome hostile to outsiders. One wonders if they've indeed retained their own"
-				+ "\nintelligence, or else if some master's hand guides them..."
+				+ "\neven appear to have some occasional (albeit rare) technological proficency.",
+				"Direct communication has proven unfruitful, as even their scouts frequently become"
+				+ "\nhostile to outsiders. One wonders if they've retained their own intelligence, or else"
+				+ "\nif some master's hand guides them..."
 			} ),
 			( "The Great War", new string[] {
-				"Where once a great civilization once dwelt on the island, now only utter ruin and"
-				+ "\ndesolation can be found. Stories tell of the old civilization discovering the power of"
-				+ "\nectoplasm, which could only be acquired from the souls of the dead.",
-				"To acquire this, a great many lives were sacrificed in the process. Fed up with being"
-				+ "\nfed on, the population revolted. Afterwards, factions formed, but they were played"
-				+ "\nagainst each other.",
-				"A cult appeared (possibly from the surviving elites of the old civilization), subverting"
-				+ "\nthe factions, and a calamity nearly ensued.",
-				"Survivors of these factions were forced to hide in the depths of the island, gradually"
-				+ "\nlosing their humanity to the powers they wielded or were wielded upon them."
+				"Where once a great civilization once dwelt on the island, now only ruin and desolation"
+				+ "\ncan be found. Stories tell of the old civilization discovering the \"power of the"
+				+ "\ndead\", a discovery that both propelled them to greatness and then to calamity.",
+				"To acquire this power, a great many lives were sacrificed in the process. At some point,"
+				+ "\ntheir population revolted. Factions formed, dynasties fell, and dangerous new powers"
+				+ "\narose.",
+				"Stories tell of \"outsiders\" suddenly appearing amidst the chaos attempting to bring"
+				+ "\ncalamity to the entire world. Their forces fed off of the ill will of the times,"
+				+ "\nand may have even had a hand in their prior instigations.",
+				"It was not until a unified effort of the warring factions did a desperate defense get"
+				+ "\nmade, but the cost was dire... both to the people, and to the land itself.",
+				"Survivors of these factions were forced to hide or flee, either slowly losing their"
+				+ "\nhumanity to the powers they once wielded, or else their ancient cultures and"
+				+ "\nidentities with their exodus. Their stories thereafter are conspiciously hidden"
+				+ "\nfrom the pages of most recorded history..."
 			} ),
-			( "The Calamity", new string[] {
-				"They say the ancient cultists nearly succeeded in summoning an elder god to Terraria"
-				+ "\nitself. A surviving faction calling themselves the Dryads sacrificed themselves to"
-				+ "\nrepel this calamity.",
-				"Did the ancient cult go into hiding? Were they themselves wiped out? None can say. All"
-				+ "\nthat's known is a recent appearance of a cult, presumably with ties to their ancient"
-				+ "\npredecesors, have begun appearing on this island now, of all times.",
-				"Could this really be a Sign of a new calamity ready to befall our world?"
+			( "The Calamity From The Stars", new string[] {
+				"They say an ancient cult nearly succeeded in summoning an evil god from beyond this world"
+				+ "\nto the land of Terraria itself. Survivors of the warring factions, pooling their"
+				+ "\nspecial knowledge and powers, made the ultimate sacrifice to defeat this foe.",
+				"But what became of the ancient cult? Did they survive the war and calamity after all this"
+				+ "\ntime? Or is it a new cult (with ties to their ancient predecesors?) that now appears on"
+				+ "\nthis island? Why now, of all times?",
+				"Could we be again doomed to repeat the mistakes of the past?"
 			} ),
 			( "The Abomination Of The Deep", new string[] {
-				"Legends tell of an abomination created from a magic spell of the Dryads to try to"
-				+ "\ncleanse the land of the Curse.",
-				"This spell was meant to draw evil spiritual energy (tainted ectoplasm) from the land and"
-				+ "\nseal it into the bowels of the world. It was cast in hopes of averting another calamity.",
-				"Sadly, the spell seems to be failing, or maybe the world itself has reached a saturation."
-				+ "\nEither way, a terrifying presence has since appeared in the depths as a result.",
-				"It dwells in the deepest reaches of the world, exuding a dark influence on the world"
-				+ "\nabove. No one knows if or when it may make its presense known to the world above..."
-				+ "\nor what calamity may result!"
+				"Legends tell of an abomination lurking in the far depths of the world, born of the great"
+				+ "\nwar and sorrows of the past.",
+				"Powerful magics meant to draw away evil from the land itself could only manage to seal it"
+				+ "\ninto the bowels of the world, hidden from sight. Alas, this temporary respite may have"
+				+ "\ngiven rise to new horrors, yet to be seen.",
+				"As the spells of old fail (or maybe simply the world itself reaches a saturation point),"
+				+ "\nthe gathered spiritual energies coallesce into a new being: A terrifying abomination"
+				+ "\nthat begins to exude its presence on the world above.",
+				"No one knows if or when it may make its presense known to the world above... nor what"
+				+ "\ndisaster may result!"
 			} ),
 			( "The Ritual", new string[] {
-				"There exists a way to directly empower a person or object with magical powers, but it"
-				+ "\nrequires ectoplasm.",
-				"It is also the main method to extract fresh ectoplasm from a person's soul in times of"
-				+ "\nantiquity; a secret now (thankfully) lost to time.",
-				"This ritual requires a focus object (usually a voodoo doll or other symbolic"
-				+ "\nrepresentation) of the person or object to act upon.",
-				"If the focus is destroyed, it dramatically releases any gathered ectoplasm with"
-				+ "\nfrightful and unpredictable results."
+				"Rediscovered from ancient times, research into the workings of the plague have revealed"
+				+ "\nthe old methods for empowering people and objects with \"magic powers\".",
+				"The cost? It requires the \"souls of the dead\", materially found in a substance known"
+				+ "\nas \"ectoplasm\".",
+				"Furthermore, the rituals speak of ways to acquire this substance via. human sacrifices."
+				+ "\nThe exact process for doing this, however, is unknown.",
+				"What is known is that a focus object (usually a \"voodoo doll\" or other symbolic"
+				+ "\ncomponent) of the person or object to act upon is needed, and will thereafter become"
+				+ "\na vital part of the channeling of magic.",
+				"If the focus is destroyed, the empowered being may \"materially manifest upon their"
+				+ "\naccumulated spiritual essence\". The exact meaning of this is unknown, but it is"
+				+ "\nthought this may be an explanation for the existance of powerful monsters or"
+				+ "\ndangerous environmental phenomena..."
 			} )
 			//The Dungeon Guardian
 			//The Dryads
