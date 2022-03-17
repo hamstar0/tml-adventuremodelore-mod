434b4c1c78025157dc955ffbe6843d0c9afc0d76
hamstar0 <webutek@gmail.com>
2022-03-17 06:12:56 -0700

    v0.20.0 - Set cyborg to be scannable for its message

--- AdventureModeLore/Tiles/FallenCyborgTile.cs
+++ AdventureModeLore/Tiles/FallenCyborgTile.cs
@@ -1,40 +1,17 @@
-﻿using Messages;
-using Microsoft.Xna.Framework;
+﻿using Microsoft.Xna.Framework;
 using Terraria;
 using Terraria.ModLoader;
 using ModLibsCore.Classes.Loadable;
+using ModLibsCore.Services.Timers;
 using ModLibsUtilityContent.Tiles;
+using Messages;
+using PKEMeter;
+using PKEMeter.Logic;
 
 
 namespace AdventureModeLore.Tiles {
 	public class MyFallenCyborgTile : ILoadable {
-		void ILoadable.OnModsLoad() { }
-
-		void ILoadable.OnModsUnload() { }
-
-		void ILoadable.OnPostModsLoad() {
-			var mytile = ModContent.GetInstance<FallenCyborgTile>();
-
-			mytile.KillMultiTile_Hook = this.KillMultiTile;
-			mytile.MouseOverFar_Hook = this.MouseOverFar;
-		}
-
-
-		////////////////
-
-		private void KillMultiTile( int x, int y, int frameX, int frameY ) {
-			for( int i = 0; i < 2; i++ ) {
-				Item.NewItem(
-					X: x * 16,
-					Y: y * 16,
-					Height: 3 * 16,
-					Width: 2 * 16,
-					Type: ModContent.ItemType<ModLibsUtilityContent.Items.MagitechScrapItem>()
-				);
-			}
-		}
-
-		private void MouseOverFar( int i, int j ) {
+		public static void ShowMessage() {
 			string id = "AML_Lore_FallenCyborg";
 			string story = "These poor guys can sometimes be found simply lying around in the deep places of the"
 					+ " island, either in disrepair, or maybe simply out of juice. Without the means of repairing"
@@ -54,6 +31,65 @@ namespace AdventureModeLore.Tiles {
 					parentMessage: MessagesAPI.StoryLoreCategoryMsg,
 					id: id
 				);
+			} );
+		}
+
+
+
+		////////////////
+
+		private bool IsHoveringCyborg = false;
+
+
+
+		////////////////
+		
+		void ILoadable.OnModsLoad() { }
+
+		void ILoadable.OnModsUnload() { }
+
+		void ILoadable.OnPostModsLoad() {
+			var mytile = ModContent.GetInstance<FallenCyborgTile>();
+
+			mytile.KillMultiTile_Hook = this.KillMultiTile;
+			mytile.MouseOverFar_Hook = this.MouseOverFar;
+
+			//
+
+			PKEMeterAPI.SetScannable(
+				name: "FallenCyborgTile",
+				scannable: new PKEScannable(
+					canScan: (x, y) => this.IsHoveringCyborg,
+					onScanCompleteAction: MyFallenCyborgTile.ShowMessage
+				),
+				allowRepeat: false,
+				runIfComplete: true
+			);
+		}
+
+
+		////////////////
+
+		private void KillMultiTile( int x, int y, int frameX, int frameY ) {
+			for( int i = 0; i < 2; i++ ) {
+				Item.NewItem(
+					X: x * 16,
+					Y: y * 16,
+					Height: 3 * 16,
+					Width: 2 * 16,
+					Type: ModContent.ItemType<ModLibsUtilityContent.Items.MagitechScrapItem>()
+				);
+			}
+		}
+
+
+		private void MouseOverFar( int i, int j ) {
+			this.IsHoveringCyborg = true;
+
+			Timers.SetTimer( "CyborgTileHover", 3, false, () => {
+				this.IsHoveringCyborg = false;
+
+				return false;
 			} );
 		}
 	}
