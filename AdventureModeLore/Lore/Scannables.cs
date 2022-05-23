2c7a0209384a16abf46fafafbf44f47aa87d6588
hamstar0 <webutek@gmail.com>
2022-05-23 16:28:24 -0700

    v0.21.2 - Set scannables to not attempt to load until in-game

--- AdventureModeLore/Lore/Scannables.cs
+++ AdventureModeLore/Lore/Scannables.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Messages;
 using ModLibsCore.Classes.Loadable;
+using ModLibsCore.Services.Hooks.LoadHooks;
 
 
 namespace AdventureModeLore.Lore {
@@ -10,21 +11,23 @@ namespace AdventureModeLore.Lore {
 		void ILoadable.OnModsUnload() { }
 
 		void ILoadable.OnPostModsLoad() {
-			Scannables.LoadScannable_CursedBones_If();
-			Scannables.LoadScannable_Dungeon_If();
-			Scannables.LoadScannable_Gems_If();
-			Scannables.LoadScannable_JungleWater_If();
-			Scannables.LoadScannable_LostExpeditions_If();
-			Scannables.LoadScannable_MagicItems_If();
-			Scannables.LoadScannable_PBGvBrambles_If();
-			Scannables.LoadScannable_Orbs_If();
-			Scannables.LoadScannable_Scaffolds_If();
-			Scannables.LoadScannable_ShadowMirror();
-			Scannables.LoadScannable_StrangePlants_If();
-			Scannables.LoadScannable_StrongGates_If();
-			Scannables.LoadScannable_UGDesert_If();
-			Scannables.LoadScannable_Warn_Jungle_If();
-			Scannables.LoadScannable_Warn_Underworld_If();
+			LoadHooks.AddWorldInPlayEachHook( () => {
+				Scannables.LoadScannable_CursedBones_If();
+				Scannables.LoadScannable_Dungeon_If();
+				Scannables.LoadScannable_Gems_If();
+				Scannables.LoadScannable_JungleWater_If();
+				Scannables.LoadScannable_LostExpeditions_If();
+				Scannables.LoadScannable_MagicItems_If();
+				Scannables.LoadScannable_PBGvBrambles_If();
+				Scannables.LoadScannable_Orbs_If();
+				Scannables.LoadScannable_Scaffolds_If();
+				Scannables.LoadScannable_ShadowMirror();
+				Scannables.LoadScannable_StrangePlants_If();
+				Scannables.LoadScannable_StrongGates_If();
+				Scannables.LoadScannable_UGDesert_If();
+				Scannables.LoadScannable_Warn_Jungle_If();
+				Scannables.LoadScannable_Warn_Underworld_If();
+			} );
 		}
 	}
 }
